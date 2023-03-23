2af10aeaceb5c0c16d62db448c5e70068685f63a
Ary0405 <aryansethia2003@gmail.com>
2023-03-24 02:22:32 +0530

    feat: contact us

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
+import Contact_us from './Pages/Contact_us';
 
 
 function App() {
   return (
     <div className="App">
-      <h1>yo</h1>
-      <p>this is a react app. <br /> its clean. <br />its fast. <br /> its responsive.</p>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/HyWYpM_S-2c" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      <p>built with ❤️ by <a href="https://github.com/PrabhavPandey" target="_blank">Prabhav Pandey</a></p>
+      <Contact_us />
     </div>
   );
 }
