7b44179fc43f8ea164e23caa2b7153a04e90f297
Ary0405 <aryansethia2003@gmail.com>
2023-03-23 23:32:57 +0530

    feat: gmail and otp verification

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth , GoogleAuthProvider} from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDEnMIhm9-1mYUOy7y4A4kHPVowIEAIM6I",
+    authDomain: "crooked-lines.firebaseapp.com",
+    projectId: "crooked-lines",
+    storageBucket: "crooked-lines.appspot.com",
+    messagingSenderId: "178059282596",
+    appId: "1:178059282596:web:c8511cdcedc846318a3e31",
+    measurementId: "G-4F94P22VRS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export {auth, provider};
