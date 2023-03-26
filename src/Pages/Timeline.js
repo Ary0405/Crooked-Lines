0c84f5ea08b5d9b88b78e3d656d267cd83e0d641
Ary0405 <aryansethia2003@gmail.com>
2023-03-27 03:15:35 +0530

    fix: timeline ui fixes

--- src/Pages/Timeline.js
+++ src/Pages/Timeline.js
@@ -5,19 +5,22 @@ import arrow from '../Assets/arrow.png'
 
 function Timeline() {
     return (
-        <div className='main_bg'>
-        <img className='timeline_image' src={background} />
-          <p><span className='heading_timeline'>have a look at the </span><span className='timeline'>Timeline</span>
-          </p>
-          <div className='flex-container'>
-          <p className='smallbox'><span className='date'>03</span><br />April 2023<br /><img className='sarrow' src={arrow} /><span className='smalltext'>call for<br />submission open</span></p>
-          <p className='smallbox2'><span className='date'>05</span><br />August 2023<br /><img className='sarrow' src={arrow} /><span className='smalltext'>submission<br />deadline</span></p>
-          <p className='smallbox2'><span className='date'>05</span><br />October 2023<br /><img className='sarrow' src={arrow} /><span className='smalltext'>longlist<br />annoucement</span></p>
-          </div>
-          <div className='flex-container'>
-          <p className='smallbox'><span className='date'>26</span><br />October 2023<br /><img className='sarrow' src={arrow} /><span className='smalltext'>shortlist<br />announcement</span></p>
-          <p className='smallbox2'><span className='date'>25</span><br />November 2023<br /><img className='sarrow' src={arrow} /><span className='smalltext'>prize giving<br />ceremony</span></p>
-          </div></div>
+        <div className='timeline_main'>
+                <img className='timeline_image' src={background} />
+            <div className='main_bg'>
+                <p><span className='heading_timeline'>have a look at the </span><span className='timeline'>Timeline</span>
+                </p>
+                <div className='flex-container'>
+                    <p className='smallbox'><span className='date'>03</span><br />April 2023<br /><img className='sarrow' src={arrow} /><span className='smalltext'>call for<br />submission open</span></p>
+                    <p className='smallbox2'><span className='date'>05</span><br />August 2023<br /><img className='sarrow' src={arrow} /><span className='smalltext'>submission<br />deadline</span></p>
+                    <p className='smallbox2'><span className='date'>05</span><br />October 2023<br /><img className='sarrow' src={arrow} /><span className='smalltext'>longlist<br />annoucement</span></p>
+                </div>
+                <div className='flex-container'>
+                    <p className='smallbox'><span className='date'>26</span><br />October 2023<br /><img className='sarrow' src={arrow} /><span className='smalltext'>shortlist<br />announcement</span></p>
+                    <p className='smallbox2'><span className='date'>25</span><br />November 2023<br /><img className='sarrow' src={arrow} /><span className='smalltext'>prize giving<br />ceremony</span></p>
+                </div>
+            </div>
+        </div>
     )
 }
 
