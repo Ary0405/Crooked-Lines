6283477657fb98bdcb4f0e1340efd9545b7c7fc0
sujith-rek <ps335@snu.edu.in>
2023-03-26 17:28:36 +0530

    added footer and other small changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import Contact_us from './Pages/Contact_us';
 import Faq from './Pages/Faq';
+import Footer from './Pages/Footer';
 import Home from './Pages/Home';
 import Sign_in from './Pages/Sign_in';
 
@@ -13,6 +14,7 @@ function App() {
       <Home />
       <Contact_us />
       <Faq />
+      <Footer/>
     </div>
   );
 }
